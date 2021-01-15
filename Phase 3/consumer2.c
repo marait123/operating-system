68e6d14946c60ce4fab88e1112e945a17afb580c
marait123 <mohammed@DESKTOP-ME73TCT.localdomain>
2021-01-15 17:49:42 +0200

    consumer2 and producer 2 working perfectly

--- Phase 3/consumer2.c
+++ Phase 3/consumer2.c
@@ -13,7 +13,7 @@
 #define MSG_SIZE 4096
 #define BUFF_SIZE 15
 // the code handles any number of clients simultaneously
-// 1. problem in removing
+
 struct msgbuff
 {
     long mtype;
@@ -31,9 +31,7 @@ int *BUFF_FULL_ADRS;
 int SEM_ID;
 void exit_handler(int sigId)
 {
-    //TODO: BUFF_FULL_ID check that is deleted
-    //TODO: check there is no reference before deletion
-    // printf("exit BUFF_FULL_ID %d", BUFF_FULL_ID);
+
     shmdt(BUFF_START_ADRS);
     shmdt(BUFF_END_ADRS);
     shmdt(BUFF_ADRS);
@@ -114,6 +112,7 @@ int main()
     // this means it is not initialized yet
     if (BUFF_ID != -1)
     {
+
         // initialize memory
         *BUFF_START_ADRS = 0;
         *BUFF_END_ADRS = 0;
@@ -137,15 +136,6 @@ int main()
         exit(-1);
     }
 
-    // debug
-    // BUFF_START_ID
-    // BUFF_END_ID
-    // BUFF_FULL_ID
-    printf("BUFF_START_ID %d \n", BUFF_START_ID);
-    printf("BUFF_END_ID %d \n", BUFF_END_ID);
-    printf("BUFF_FULL_ID %d \n", BUFF_FULL_ID);
-    printf("BUFF_ID %d \n", BUFF_ID);
-    //end
     up_q_id = msgget(up_q_key, 0666 | IPC_CREAT);
     down_q_id = msgget(down_q_key, 0666 | IPC_CREAT);
     if (up_q_id == -1 || down_q_id == -1)
@@ -158,7 +148,6 @@ int main()
     printf("BUFF_ID  = %d\n", BUFF_ID);
 
     // initialize the semaphore
-    // TODO:
     int buff_number = 0;
     struct msgbuff message;
     // producer writes buff_number starting from the buff_end
@@ -169,59 +158,31 @@ int main()
     passing) to the producer telling it that the buffer is no longer full.
     • If the buffer is neither empty nor full, it consumes an item from the buffer
     */
-    // TODO: problem when producer is faster than consumer
-    //TODO: check that more than one producer and cosumers are allowed
+
+    // TODO: what happens if consumer is started first
     while (1)
     {
-
         // check if buffer is empty
         down(SEM_ID);
-        printf("semaphore in producer\n");
+        printf("semaphore in consumer\n");
         // if buffer empty
         int count = *BUFF_FULL_ADRS;
         if (count == 0)
         {
 
             printf("buffer is empty\n");
-            int *target_addr = BUFF_ADRS + *BUFF_END_ADRS;
-            *target_addr = buff_number;
-            buff_number++;
-            (*BUFF_END_ADRS) = ((*BUFF_END_ADRS) + 1) % BUFF_SIZE;
-            message.mtype = 0;
-            strcpy(message.mtext, "not_empty");
-            up(SEM_ID);
-            send_val = msgsnd(up_q_id, &message, sizeof(message.mtext), !IPC_NOWAIT);
-            down(SEM_ID);
-            (*BUFF_FULL_ADRS)++; // put here for a reason (so that the message is consumed)
-        }
-        else if (count == BUFF_SIZE)
-        {
-            printf("buffer is full\n");
-            message.mtype = 0;
-            up(SEM_ID);
-            rec_val = msgrcv(down_q_id, &message, sizeof(message.mtext), 0, !IPC_NOWAIT);
-            down(SEM_ID);
-            if (rec_val == -1)
-            {
-                perror("Error in receive\n");
-                exit(-1);
-            }
-            printf("something was consumed\n");
-            // buff_number++;
         }
         else
         {
             printf("buffer is neither empty nor full\n");
-            int *target_addr = BUFF_ADRS + *BUFF_END_ADRS;
-            *target_addr = buff_number;
-            buff_number++;
-            (*BUFF_END_ADRS) = ((*BUFF_END_ADRS) + 1) % BUFF_SIZE;
-            (*BUFF_FULL_ADRS)++;
+            int *target_addr = BUFF_ADRS + *BUFF_START_ADRS;
+            (*BUFF_START_ADRS) = ((*BUFF_START_ADRS) + 1) % BUFF_SIZE;
+            *BUFF_FULL_ADRS -= 1;
+            printf("number consumed %d \n", *target_addr);
         }
         printf("number of items %d\n", count);
-        printf("current buff_number %d\n", buff_number);
-        sleep(4);
         up(SEM_ID);
+        sleep(1);
     }
     return 0;
 }
